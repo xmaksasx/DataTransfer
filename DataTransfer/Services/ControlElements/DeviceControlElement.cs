53cfdaf8064725b2495c0861df5b4027d50e65e9
XmaksasX <XmaksasX@gmail.com>
2021-05-20 17:11:54 +0300

    Add project files.

---
+++ DataTransfer/Services/ControlElements/DeviceControlElement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharpDX.DirectInput;
+
+namespace DataTransfer.Services.ControlElements
+{
+	class DeviceControlElement
+	{
+		private DirectInput _directInput = new DirectInput();
+		private List<Joystick> _joysticks = new List<Joystick>();
+		private List<DeviceInstance> _deviceInstances = new List<DeviceInstance>();
+
+		private Joystick SetDevice(Guid guidJoystick)
+		{
+			var joystick = new Joystick(_directInput, guidJoystick);
+			joystick.Properties.BufferSize = 128;
+			joystick.Acquire();
+			return joystick;
+		}
+
+		private List<DeviceInstance> SearchJoystick()
+		{
+			return _directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices).ToList();
+		}
+
+		public JoystickState ReadData(Guid guid)
+		{
+			foreach (var joystick in _joysticks)
+				if (guid == joystick.Information.ProductGuid)
+					return joystick.GetCurrentState();
+
+			return new JoystickState();
+		}
+
+		public void AddJoystick(string guid)
+		{
+			Guid guidDevice = new Guid(guid);
+			foreach (var deviceInstance in _deviceInstances)
+				if (guidDevice == deviceInstance.ProductGuid)
+					_joysticks.Add(SetDevice(guidDevice));
+		}
+	}
+}
---
+++ JoystickExample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharpDX.DirectInput;
+
+namespace JoystickExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+			// Initialize DirectInput
+			var directInput = new DirectInput();
+			var devices = SearchDevice(directInput);
+			var joystickState = new JoystickState();
+
+			// Find a Joystick Guid
+			var joystickGuid = Guid.Empty;
+			var joystickName = "";
+
+			foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad,
+				DeviceEnumerationFlags.AllDevices))
+			{
+				joystickGuid = deviceInstance.InstanceGuid;
+				joystickName = deviceInstance.ProductName;
+			}
+
+			// If Gamepad not found, look for a Joystick
+			if (joystickGuid == Guid.Empty)
+				foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick,
+					DeviceEnumerationFlags.AllDevices))
+				{
+					joystickGuid = deviceInstance.InstanceGuid;
+					joystickName = deviceInstance.InstanceName;
+					Console.WriteLine(deviceInstance.ProductGuid);
+				}
+
+			// If Joystick not found, throws an error
+			if (joystickGuid == Guid.Empty)
+			{
+				Console.WriteLine("No joystick/Gamepad found.");
+				Console.ReadKey();
+				Environment.Exit(1);
+			}
+
+			// Instantiate the joystick
+			var joystick = new Joystick(directInput, joystickGuid);
+			joystick.Properties.BufferSize = 128;
+			joystick.Acquire();
+
+			while (true)
+			{
+				//joystick.Poll();
+			var t=	joystick.GetCurrentState();
+			var t2 = joystick.Information;
+			var t1 = joystick;
+			//var datas = joystick.GetBufferedData();
+			//foreach (var state in datas)
+			//	Console.WriteLine(state);
+			}
+        }
+
+		private static void Init(string guidRus, string guidRud)
+		{
+			//return directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices).ToList();
+		}
+		private static List<DeviceInstance> SearchDevice(DirectInput directInput)
+		{
+			return directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices).ToList();
+		}
+	}
+
+}
