8da803d0460f840f2b75e70fb174dbe74150ae79
XmaksasX <XmaksasX@gmail.com>
2021-05-20 20:07:41 +0300

    Создание инфраструктуры приложения

---
+++ DataTransfer/Model/SimulationManagement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransfer.Model
+{
+	class SimulationManagement
+	{
+		// 0 - стоп
+		// 1 - старт
+		// 2 - пауза
+		// Команда на управление
+		private double ModeState;
+
+		// 0 - автопилот
+		// 1 - сеть
+		// 2 - джойстик НИИЭС
+		// 3 - локальный джойстик
+		// Тип огранов управления
+		private double TypeCockpit;
+
+	}
+}
---
+++ DataTransfer/ViewModel/Base/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataTransfer.ViewModel.Base
+{
+	internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		private bool _disposed;
+		protected virtual void Dispose(bool disposing)
+		{
+			if (_disposed || disposing) return;
+			_disposed = true;
+			//Освобождение управляемых ресурсов
+		}
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+
+	}
+}
